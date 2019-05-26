64bc0172c36548772c46b9c09d2b46ea257c1b97
Łukasz Malicki <qweluke@ukaszs-MacBook-Pro.local>
2019-05-26 13:48:50 +0200

    first commit

---
+++ dist/cjs/mycomponent.cjs.js
@@ -0,0 +1,7 @@
+'use strict';
+
+const __chunk_1 = require('./mycomponent-7b1e16d0.js');
+
+__chunk_1.patchBrowser().then(resourcesUrl => {
+  return __chunk_1.bootstrapLazy([["polocode-button_3.cjs",[[1,"polocode-button",{"type":[1],"disabled":[4],"class":[1],"text":[1]}],[1,"polocode-input",{"type":[1],"disabled":[4],"required":[4],"labelTxt":[1,"label-txt"],"class":[1],"value":[1],"placeholder":[1],"error":[1],"id":[1]}],[1,"polocode-modal",{"id":[1],"isVisible":[516,"is-visible"]}]]]], { resourcesUrl });
+});
---
+++ dist/esm/mycomponent.mjs
@@ -0,0 +1,5 @@
+import { a as patchBrowser, b as bootstrapLazy } from './mycomponent-6a942625.js';
+
+patchBrowser().then(resourcesUrl => {
+  return bootstrapLazy([["polocode-button_3",[[1,"polocode-button",{"type":[1],"disabled":[4],"class":[1],"text":[1]}],[1,"polocode-input",{"type":[1],"disabled":[4],"required":[4],"labelTxt":[1,"label-txt"],"class":[1],"value":[1],"placeholder":[1],"error":[1],"id":[1]}],[1,"polocode-modal",{"id":[1],"isVisible":[516,"is-visible"]}]]]], { resourcesUrl });
+});
